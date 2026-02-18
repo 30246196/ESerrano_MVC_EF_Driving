a7adf2bdd39f2e51f7b64324dd075878e26b3500
30246196 <edithserra1@gmail.com>
2026-02-18 11:19:10 +0000

    Add project files.

---
+++ ESerrano_MVC_EF_Driving/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ESerrano_MVC_EF_Driving
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ESerrano_MVC_EF_Driving/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ESerrano_MVC_EF_Driving.MvcApplication" Language="C#" %>
